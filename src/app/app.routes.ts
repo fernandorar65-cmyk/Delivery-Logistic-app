b55f2f72c2da7ca9b88c1c5986500c07691ceb92
Fernando-Pecano <faquino@pecano.pe>
2026-01-11 23:54:08 -0500

    fix: budget fo maxin bytes length

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,6 @@
 import { Routes } from '@angular/router';
-import { ClientListViewComponent } from './views/clients/client-list-view/client-list-view.component';
-import { ClientDetailViewComponent } from './views/clients/client-detail-view/client-detail-view.component';
-import { ClientFormViewComponent } from './views/clients/client-form-view/client-form-view.component';
-import { DriverListViewComponent } from './views/drivers/driver-list-view/driver-list-view.component';
-import { DriverDetailViewComponent } from './views/drivers/driver-detail-view/driver-detail-view.component';
-import { DriverFormViewComponent } from './views/drivers/driver-form-view/driver-form-view.component';
-import { InternalClientListViewComponent } from './views/internal-clients/internal-client-list-view/internal-client-list-view.component';
-import { InternalClientDetailViewComponent } from './views/internal-clients/internal-client-detail-view/internal-client-detail-view.component';
-import { InternalClientFormViewComponent } from './views/internal-clients/internal-client-form-view/internal-client-form-view.component';
-import { UserListViewComponent } from './views/users/user-list-view/user-list-view.component';
-import { UserDetailViewComponent } from './views/users/user-detail-view/user-detail-view.component';
-import { UserFormViewComponent } from './views/users/user-form-view/user-form-view.component';
-import { OperationListViewComponent } from './views/operations/operation-list-view/operation-list-view.component';
-import { OperationDetailViewComponent } from './views/operations/operation-detail-view/operation-detail-view.component';
-import { OperationFormViewComponent } from './views/operations/operation-form-view/operation-form-view.component';
-import { OrderListViewComponent } from './views/orders/order-list-view/order-list-view.component';
-import { OrderDetailViewComponent } from './views/orders/order-detail-view/order-detail-view.component';
-import { OrderFormViewComponent } from './views/orders/order-form-view/order-form-view.component';
+// Componentes que se cargan siempre (eager loading)
 import { LoginViewComponent } from './views/login/login-view.component';
-import { DashboardViewComponent } from './views/dashboard/dashboard-view.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
@@ -37,103 +19,128 @@ export const routes: Routes = [
     children: [
       {
         path: 'dashboard',
-        component: DashboardViewComponent
+        loadComponent: () => import('./views/dashboard/dashboard-view.component')
+          .then(m => m.DashboardViewComponent)
       },
       {
         path: 'clients',
-        component: ClientListViewComponent
+        loadComponent: () => import('./views/clients/client-list-view/client-list-view.component')
+          .then(m => m.ClientListViewComponent)
       },
       {
         path: 'clients/new',
-        component: ClientFormViewComponent
+        loadComponent: () => import('./views/clients/client-form-view/client-form-view.component')
+          .then(m => m.ClientFormViewComponent)
       },
       {
         path: 'clients/:id',
-        component: ClientDetailViewComponent
+        loadComponent: () => import('./views/clients/client-detail-view/client-detail-view.component')
+          .then(m => m.ClientDetailViewComponent)
       },
       {
         path: 'clients/:id/edit',
-        component: ClientFormViewComponent
+        loadComponent: () => import('./views/clients/client-form-view/client-form-view.component')
+          .then(m => m.ClientFormViewComponent)
       },
       {
         path: 'drivers',
-        component: DriverListViewComponent
+        loadComponent: () => import('./views/drivers/driver-list-view/driver-list-view.component')
+          .then(m => m.DriverListViewComponent)
       },
       {
         path: 'drivers/new',
-        component: DriverFormViewComponent
+        loadComponent: () => import('./views/drivers/driver-form-view/driver-form-view.component')
+          .then(m => m.DriverFormViewComponent)
       },
       {
         path: 'drivers/:id',
-        component: DriverDetailViewComponent
+        loadComponent: () => import('./views/drivers/driver-detail-view/driver-detail-view.component')
+          .then(m => m.DriverDetailViewComponent)
       },
       {
         path: 'drivers/:id/edit',
-        component: DriverFormViewComponent
+        loadComponent: () => import('./views/drivers/driver-form-view/driver-form-view.component')
+          .then(m => m.DriverFormViewComponent)
       },
       {
         path: 'internal-clients',
-        component: InternalClientListViewComponent
+        loadComponent: () => import('./views/internal-clients/internal-client-list-view/internal-client-list-view.component')
+          .then(m => m.InternalClientListViewComponent)
       },
       {
         path: 'internal-clients/new',
-        component: InternalClientFormViewComponent
+        loadComponent: () => import('./views/internal-clients/internal-client-form-view/internal-client-form-view.component')
+          .then(m => m.InternalClientFormViewComponent)
       },
       {
         path: 'internal-clients/:id',
-        component: InternalClientDetailViewComponent
+        loadComponent: () => import('./views/internal-clients/internal-client-detail-view/internal-client-detail-view.component')
+          .then(m => m.InternalClientDetailViewComponent)
       },
       {
         path: 'internal-clients/:id/edit',
-        component: InternalClientFormViewComponent
+        loadComponent: () => import('./views/internal-clients/internal-client-form-view/internal-client-form-view.component')
+          .then(m => m.InternalClientFormViewComponent)
       },
       {
         path: 'users',
-        component: UserListViewComponent
+        loadComponent: () => import('./views/users/user-list-view/user-list-view.component')
+          .then(m => m.UserListViewComponent)
       },
       {
         path: 'users/new',
-        component: UserFormViewComponent
+        loadComponent: () => import('./views/users/user-form-view/user-form-view.component')
+          .then(m => m.UserFormViewComponent)
       },
       {
         path: 'users/:id',
-        component: UserDetailViewComponent
+        loadComponent: () => import('./views/users/user-detail-view/user-detail-view.component')
+          .then(m => m.UserDetailViewComponent)
       },
       {
         path: 'users/:id/edit',
-        component: UserFormViewComponent
+        loadComponent: () => import('./views/users/user-form-view/user-form-view.component')
+          .then(m => m.UserFormViewComponent)
       },
       {
         path: 'operations',
-        component: OperationListViewComponent
+        loadComponent: () => import('./views/operations/operation-list-view/operation-list-view.component')
+          .then(m => m.OperationListViewComponent)
       },
       {
         path: 'operations/new',
-        component: OperationFormViewComponent
+        loadComponent: () => import('./views/operations/operation-form-view/operation-form-view.component')
+          .then(m => m.OperationFormViewComponent)
       },
       {
         path: 'operations/:id',
-        component: OperationDetailViewComponent
+        loadComponent: () => import('./views/operations/operation-detail-view/operation-detail-view.component')
+          .then(m => m.OperationDetailViewComponent)
       },
       {
         path: 'operations/:id/edit',
-        component: OperationFormViewComponent
+        loadComponent: () => import('./views/operations/operation-form-view/operation-form-view.component')
+          .then(m => m.OperationFormViewComponent)
       },
       {
         path: 'orders',
-        component: OrderListViewComponent
+        loadComponent: () => import('./views/orders/order-list-view/order-list-view.component')
+          .then(m => m.OrderListViewComponent)
       },
       {
         path: 'orders/new',
-        component: OrderFormViewComponent
+        loadComponent: () => import('./views/orders/order-form-view/order-form-view.component')
+          .then(m => m.OrderFormViewComponent)
       },
       {
         path: 'orders/:id',
-        component: OrderDetailViewComponent
+        loadComponent: () => import('./views/orders/order-detail-view/order-detail-view.component')
+          .then(m => m.OrderDetailViewComponent)
       },
       {
         path: 'orders/:id/edit',
-        component: OrderFormViewComponent
+        loadComponent: () => import('./views/orders/order-form-view/order-form-view.component')
+          .then(m => m.OrderFormViewComponent)
       },
       {
         path: '',
