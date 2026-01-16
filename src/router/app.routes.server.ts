a648e4355a49dafd03bd2a0f9bca2c389a07bf70
Fernando-Pecano <faquino@pecano.pe>
2026-01-15 21:39:17 -0500

    delete: eliminar rutas que ya no se usan

--- src/router/app.routes.server.ts
+++ src/router/app.routes.server.ts
@@ -23,47 +23,11 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Prerender
   },
   {
-    path: 'drivers',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'drivers/new',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'internal-clients',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'internal-clients/new',
-    renderMode: RenderMode.Prerender
-  },
-  {
     path: 'users',
     renderMode: RenderMode.Prerender
   },
   {
     path: 'users/new',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'operations',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'operations/new',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'orders',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'orders/new',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'orders/assignment',
     renderMode: RenderMode.Prerender
   },
   {
@@ -84,43 +48,11 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
-    path: 'drivers/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'drivers/:id/edit',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'internal-clients/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'internal-clients/:id/edit',
-    renderMode: RenderMode.Server
-  },
-  {
     path: 'users/:id',
     renderMode: RenderMode.Server
   },
   {
     path: 'users/:id/edit',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'operations/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'operations/:id/edit',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'orders/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'orders/:id/edit',
     renderMode: RenderMode.Server
   },
   {
--- src/router/app.routes.ts
+++ src/router/app.routes.ts
@@ -67,46 +67,6 @@ export const routes: Routes = [
           .then(m => m.ClientFormViewComponent)
       },
       {
-        path: 'drivers',
-        loadComponent: () => import('../app/views/drivers/driver-list-view/driver-list-view.component')
-          .then(m => m.DriverListViewComponent)
-      },
-      {
-        path: 'drivers/new',
-        loadComponent: () => import('../app/views/drivers/driver-form-view/driver-form-view.component')
-          .then(m => m.DriverFormViewComponent)
-      },
-      {
-        path: 'drivers/:id',
-        loadComponent: () => import('../app/views/drivers/driver-detail-view/driver-detail-view.component')
-          .then(m => m.DriverDetailViewComponent)
-      },
-      {
-        path: 'drivers/:id/edit',
-        loadComponent: () => import('../app/views/drivers/driver-form-view/driver-form-view.component')
-          .then(m => m.DriverFormViewComponent)
-      },
-      {
-        path: 'internal-clients',
-        loadComponent: () => import('../app/views/internal-clients/internal-client-list-view/internal-client-list-view.component')
-          .then(m => m.InternalClientListViewComponent)
-      },
-      {
-        path: 'internal-clients/new',
-        loadComponent: () => import('../app/views/internal-clients/internal-client-form-view/internal-client-form-view.component')
-          .then(m => m.InternalClientFormViewComponent)
-      },
-      {
-        path: 'internal-clients/:id',
-        loadComponent: () => import('../app/views/internal-clients/internal-client-detail-view/internal-client-detail-view.component')
-          .then(m => m.InternalClientDetailViewComponent)
-      },
-      {
-        path: 'internal-clients/:id/edit',
-        loadComponent: () => import('../app/views/internal-clients/internal-client-form-view/internal-client-form-view.component')
-          .then(m => m.InternalClientFormViewComponent)
-      },
-      {
         path: 'users',
         loadComponent: () => import('../app/views/users/user-list-view/user-list-view.component')
           .then(m => m.UserListViewComponent)
@@ -125,51 +85,6 @@ export const routes: Routes = [
         path: 'users/:id/edit',
         loadComponent: () => import('../app/views/users/user-form-view/user-form-view.component')
           .then(m => m.UserFormViewComponent)
-      },
-      {
-        path: 'operations',
-        loadComponent: () => import('../app/views/operations/operation-list-view/operation-list-view.component')
-          .then(m => m.OperationListViewComponent)
-      },
-      {
-        path: 'operations/new',
-        loadComponent: () => import('../app/views/operations/operation-form-view/operation-form-view.component')
-          .then(m => m.OperationFormViewComponent)
-      },
-      {
-        path: 'operations/:id',
-        loadComponent: () => import('../app/views/operations/operation-detail-view/operation-detail-view.component')
-          .then(m => m.OperationDetailViewComponent)
-      },
-      {
-        path: 'operations/:id/edit',
-        loadComponent: () => import('../app/views/operations/operation-form-view/operation-form-view.component')
-          .then(m => m.OperationFormViewComponent)
-      },
-      {
-        path: 'orders',
-        loadComponent: () => import('../app/views/orders/order-list-view/order-list-view.component')
-          .then(m => m.OrderListViewComponent)
-      },
-      {
-        path: 'orders/new',
-        loadComponent: () => import('../app/views/orders/order-form-view/order-form-view.component')
-          .then(m => m.OrderFormViewComponent)
-      },
-      {
-        path: 'orders/:id',
-        loadComponent: () => import('../app/views/orders/order-detail-view/order-detail-view.component')
-          .then(m => m.OrderDetailViewComponent)
-      },
-      {
-        path: 'orders/:id/edit',
-        loadComponent: () => import('../app/views/orders/order-form-view/order-form-view.component')
-          .then(m => m.OrderFormViewComponent)
-      },
-      {
-        path: 'orders/assignment',
-        loadComponent: () => import('../app/views/orders/order-assignment-view/order-assignment-view.component')
-          .then(m => m.OrderAssignmentViewComponent)
       },
       {
         path: '',
