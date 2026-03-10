25846893078736b5d19df91afaabe2d9c0d556c7
Fernando-Pecano <faquino@pecano.pe>
2026-03-09 21:37:58 -0500

    add: new functions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://prologistic-api-dev.up.railway.app/api/v1'
-  apiUrl: 'http://localhost:8000/api/v1',
+  apiUrl: 'https://prologistic-api-dev.up.railway.app/api/v1'
+  //apiUrl: 'http://localhost:8000/api/v1',
   /** URL base del servicio de optimización de rutas (sin token). Definir en build/deploy. */
   routeOptimizationApiUrl: ''
 };
